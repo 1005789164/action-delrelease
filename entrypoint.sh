a79a603da20bc0bdbc2d98d62993d3019a76e9f5
MJ <1005789164@qq.com>
2020-07-16 08:44:12 +0800

    modify

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,24 +28,24 @@ fi
 ISTAG="$(echo ${ISTAG} | tr -d \")"
 
 BASE_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases"
-
-printf "\n000000000000000000000000000\n\n"
+echo $NAME
 if [[ "${NAME}" != *"all"* ]] || [[ "${NAME}" != *"ALL"* ]]; then
   BASE_URL=${BASE_URL}/tags
-  printf "\n111111111111111111111111111111\n\n"
   for entry in "$(echo ${NAME} | tr ' ' '\n')"; do
-  printf "\n2222222222222222222222222\n\n"
     RELEASE_URL="$(curl -sS -H "Authorization: token ${TOKEN}" \
       ${BASE_URL}/$entry | jq -r '.url | select(. != null)')"
     curl -sS -H "Authorization: token ${TOKEN}" \
     -X DELETE \
     $RELEASE_URL
 
+    printf "\n111111111111111111111111111111\n\n"
     if [ ${ISTAG} == "yes" ] || [ ${ISTAG} == "YES" ]; then
       curl -sS -H "Authorization: token ${TOKEN}" \
       -X DELETE \
       https://api.github.com/repos/${GITHUB_REPOSITORY}/git/refs/tags/$entry
+	printf "\n2222222222222111\n\n"
     fi
+    printf "\n333333333311111\n\n"
   done
 else
   CODE="$(curl -sS -H "Authorization: token ${TOKEN}" \
